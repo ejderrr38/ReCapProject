cdc53400f7602a91c0f19f28c2a84e68350a3e26
Zekeriya İshak DOĞANAY <zekishakdgny38@gmail.com>
2021-02-03 20:13:30 +0300

    Add project files.

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Car:IEntity
+    {
+        public int CarId { get; set; }
+        
+        //brand-marka
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public int ModelYear { get; set; }
+        //dailyprice- günlük fiyatı
+        public double DailyPrice { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
